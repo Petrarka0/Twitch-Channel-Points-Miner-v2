b77575a5133aac562965b0a5e41d5709f4b6bb3b
Petrarka0 <ryabovp79@gmail.com>
2024-04-11 14:34:16 +0300

    proibably fix multipule CTRL + C breaking exit sequence #484

--- TwitchChannelPointsMiner/TwitchChannelPointsMiner.py
+++ TwitchChannelPointsMiner/TwitchChannelPointsMiner.py
@@ -408,6 +408,8 @@ class TwitchChannelPointsMiner:
                             )
 
     def end(self, signum, frame):
+        if not self.running:
+            return
         logger.info("CTRL+C Detected! Please wait just a moment!")
 
         for streamer in self.streamers:
